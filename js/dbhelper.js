4ee4da2343391d6f42efea16b6a053150c6b47cf
Jimmy Mercado <jimmy.mercado@live.com>
2018-06-08 23:42:08 -0700

    moving service worker script to dbHelper.js and correcting heading tags

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -171,3 +171,17 @@ class DBHelper {
   }
 
 }
+
+
+/* Adding Service Worker */
+if('serviceWorker' in navigator){
+
+  navigator.serviceWorker    
+      .register('/service-worker.js', {scope: '/'})
+      .then(function(registration){
+          console.log('ServiceWorker Registered');
+      })
+      .catch(function(err){
+          console.log('ServiceWorker Unsupported', err);
+      })
+}
--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ createRestaurantHTML = (restaurant) => {
 
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
